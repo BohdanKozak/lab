bb91394e520865c5a1dbeefc85ef4cb07dc207ae
Bohdan Kozak <bohdan.kozak.pm.2021@lpnu.ua>
2022-08-30 22:46:58 +0300

    LUCIK

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,14 @@
-const {src, dest, watch, series, parallel} = require("gulp");
-const browserSync = require("browser-sync").create();
-
-const htmlmin = require("gulp-htmlmin");
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
 function watch(){
     browserSync.init({
         server:{
             baseDir: './'
         }
     })
-    gulp.watch('./scss/**/*.css ');
+    gulp.watch('*.css').on('change', browserSync.reload);
     gulp.watch('./*.html' ).on('change', browserSync.reload);
     gulp.watch('./js/**/*.js ').on('change', browserSync.reload);
+
 }
-const {series} = require("gulp");
-const test = cb =>{
-    console.log("test message");
-    cb();
-}
-exports.default = series(test)
 exports.watch = watch;
