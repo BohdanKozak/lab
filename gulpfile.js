6fb689bf63353676a08bea1f16693bc0ad4b2c20
Bohdan Kozak <bohdan.kozak.pm.2021@lpnu.ua>
2022-10-05 13:37:24 +0300

    new commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
+const pump = require("pump");
+const clean = require('gulp-clean');
 function watch(){
     browserSync.init({
         server:{
@@ -12,3 +15,14 @@ function watch(){
 
 }
 exports.watch = watch;
+function sass_toCSS_task(cb) {
+    pump([
+
+        gulp.src("sass/*.scss"),//створюємо потік
+        sass().on('error', sass.logError),//перекомпільовуємо scss у css
+        gulp.dest("css/")//куди копіюємо 
+    ],
+        cb
+    );
+}
+    exports.sassToCSS = sass_toCSS_task;
--- index.js
+++ index.js
@@ -1,147 +1,208 @@
 window.onload = function () {
-	var chart = new CanvasJS.Chart("chartContainer", {
-       
-		theme: "light2",//light1
-        zoomEnabled: true,
-        panEnabled: true,
-		
-        creditText: "Custom String",
-        culture:"es",
-		
-		title:{
-			              
+
+	const labels = ['Ja', 'Fe', 'Ma', 'Ap', 'Ma', 'Ju', 'Ji', 'Au', 'Se', 'Oc', 'No', 'De'];
+
+	var data = {
+		labels: labels,
+		datasets: [{
+			backgroundColor: 'rgba(89,199,255,0.2)',
+			borderColor: 'rgb(89, 199, 255)',
+			data: [100, 110, 220, 250, 210, 215, 270, 290, 270, 190, 220, 100],
+			pointRadius: 0,
+			fill: 1,
+                
+		}, {
+			backgroundColor: 'rgb(225, 225, 225)',
+			borderColor: 'rgb(225, 225, 225)',
+			data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			pointRadius: 0,
+
+		}, {
+			backgroundColor: 'rgba(42,164,137,0.2)',
+			borderColor: 'rgb(42, 164, 137)',
+			data: [110, 170, 220, 300, 250, 260, 280, 275, 270, 250, 190, 110],
+			pointRadius: 0,
+			fill: 1,
+
+		}, {
+			backgroundColor: 'rgb(225, 225, 225)',
+			borderColor: 'rgb(225, 225, 225)',
+			data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			pointRadius: 0,
+
+		}]
+	};
+
+	var config_1 = {
+		type: 'line',
+		data: data,
+		options: {
+			plugins: {
+				legend: {
+					display: false
+				}
+			},
+			scales: {
+				y: {
+					max: 500,
+					min: 0,
+					grace: '5%'
+				}
+			}
 		},
-		data: [              
-		{
-           
-			type: "area",
-			fillOpacity: .2,
-            color:"aqua",
-			cursor: "pointer",
-			markerType: "none",
-			lineThickness: 3,
-			dataPoints: [
-				{ label: "Ja",  y: 150  },
-				{ label: "Fe", y: 155  },
-				{ label: "Ma", y:  270 },
-				{ label: "Ap",  y: 250  },
-				{ label: "Ma",  y: 230  },
-                { label: "Ju",  y: 240  },
-                { label: "Ji",  y: 270  },
-                { label: "Au",  y: 280  },
-                { label: "Se",  y: 300  },
-                { label: "Oc",  y: 170  },
-                { label: "No",  y: 180  },
-                { label: "De",  y: 150  },
-			]
-            
-		},{
-            type: "area",
-            color:"lightgreen",
-			fillOpacity: .2,
-			markerType: "none",
-			cursor: "pointer",
-			dataPoints: [
-				{ label: "Ja",  y: 150  },
-				{ label: "Fe", y: 175  },
-				{ label: "Ma", y:  270 },
-				{ label: "Ap",  y: 300  },
-				{ label: "Ma",  y: 250  },
-                { label: "Ju",  y: 260  },
-                { label: "Ji",  y: 280  },                
-                { label: "Au",  y: 280  },
-                { label: "Se",  y: 270  },
-                { label: "Oc",  y: 190  },
-                { label: "No",  y: 170  },
-                { label: "De",  y: 160  },
-			]
-        }
-		],			
-		
-	});
-	chart.render();
+             
+	};
+	const data_2 = {
+		labels: labels,
+		datasets: [{
+			backgroundColor: 'rgba(89,199,255,0.2)',
+			borderColor: 'rgb(89, 199, 255)',
+			data: [140, 135, 130, 125, 140, 145, 150, 145, 150, 140, 200, 300],
+			pointRadius: 0,
+			fill: 1,
+                
+		}, {
+			backgroundColor: 'rgb(225, 225, 225)',
+			borderColor: 'rgb(225, 225, 225)',
+			data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			pointRadius: 0,
 
-	var chart_2 = new CanvasJS.Chart("chartContainer_2", {
-       
-		theme: "light2",//light1
-        zoomEnabled: true,
-        panEnabled: true,
-		
-        creditText: "Custom String",
-        culture:"es",
-		
-		title:{
-			              
+		}, {
+			backgroundColor: 'rgba(42,164,137,0.2)',
+			borderColor: 'rgb(42, 164, 137)',
+			data: [150, 170, 165, 160, 157, 170, 200, 160, 150, 170, 180, 190],
+			pointRadius: 0,
+			fill: 1,
+
+		}, {
+			backgroundColor: 'rgb(225, 225, 225)',
+			borderColor: 'rgb(225, 225, 225)',
+			data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			pointRadius: 0,
+
+		}]
+	};
+	const config_2 = {
+		type: 'line',
+		data: data_2,
+		options: {
+			plugins: {
+				legend: {
+					display: false
+				}
+			},
+			scales: {
+				y: {
+					max: 600,
+					min: 0,
+					grace: '5%'
+				}
+			}
 		},
-		data: [              
-		{
-           
-			type: "area",
-			fillOpacity: .2,
-            color:"aqua",
-			cursor: "pointer",
-			markerType: "none",
-			lineThickness: 3,
-			dataPoints: [
-				
+             
+	};
+	const myChart = new Chart(
+		document.getElementById('myChart_1'),
+		config_1
+	);
+	const myChart_2 = new Chart(
+		document.getElementById('myChart_2'),
+		config_2
+	)
+	  
+	var data_5 = {
+		datasets: [
+			{
+				data: [76, 289],
+				backgroundColor: [
+					"#2196F3",
+					"#b5bfcd",
+                            
+				],
+				hoverBackgroundColor: [
+					"#2196F3",
+						"#b5bfcd",
+                            
+				]
+			}]
+	};
+	const config_4 = {
+		type: 'doughnut',
+		data: data_5,
+		options: {
+			responsive: true,
+			plugins: {
+				legend: {
+					display: false,
+				},
+				title: {
+					display: false,
+					
+				}
+			}
+		},
+	};
+	const myChart_4 = new Chart(
+		document.getElementById('myChart_3'),
+		config_4)
+setInterval(function() {
+	let today = new Date();
+	let hour = today.getHours();
+	let min = today.getMinutes();
+	let sec = today.getSeconds();
+	am_pm = "AM";
 
-				{ label: "Ja",  y: 130  },
-				{ label: "Fe", y: 125  },
-				{ label: "Ma", y:  120 },
-				{ label: "Ap",  y: 115  },
-				{ label: "Ma",  y: 140  },
-                { label: "Ju",  y: 145  },
-                { label: "Ji",  y: 150  },
-                { label: "Au",  y: 145  },
-                { label: "Se",  y: 150  },
-                { label: "Oc",  y: 140  },
-                { label: "No",  y: 240  },
-                { label: "De",  y: 240  },
-			]
-            
-		},{
-            type: "area",
-            color:"lightgreen",
-			fillOpacity: .2,
-			markerType: "none",
-			cursor: "pointer",
-			dataPoints: [
-				{ label: "Ja",  y: 150  },
-				{ label: "Fe", y: 170  },
-				{ label: "Ma", y:  170 },
-				{ label: "Ap",  y: 175  },
-				{ label: "Ma",  y: 187  },
-                { label: "Ju",  y: 195  },
-                { label: "Ji",  y: 200  },                
-                { label: "Au",  y: 170  },
-                { label: "Se",  y: 150  },
-                { label: "Oc",  y: 175  },
-                { label: "No",  y: 180  },
-                { label: "De",  y: 183  },
-			]
-        }
-		],			
-		
-	});
-	chart_2.render();
+	if (hour > 12) {
+		hour -= 12;
+		am_pm = "PM";
+	}
+	if (hour == 0) {
+		hour = 12;
+		am_pm = "AM";
+	}
 
-	var chart_3 = new CanvasJS.Chart("doughnut",
-    {
-      title:{
-        
-      },
-      data: [
-      {
-       type: "doughnut",
-	   color:"#53c6ea",
-       dataPoints: [
-       {  y: 65},  
-       {  y: 300},
-       ]
-     }
-     ]
-   });
+	hour = hour < 10 ? "0" + hour : hour;
+	min = min < 10 ? "0" + min : min;
+	sec = sec < 10 ? "0" + sec : sec;
+	let currentTime = hour + ":"
+			+ min + ":" + sec + " " + am_pm;
 
-   chart_3.render();
-};
+	document.getElementById("time")
+			.innerHTML = currentTime;
+}, 1000)};
+setInterval(function() {
+	let today = new Date();
+	let month = today.getMonth() + 1;
+	let day = today.getDate();
+	let month2;
+	month = month < 10 ? "0" + month : month;
+	day = day < 10 ? "0" + day : day;
+	if (month === "1") {
+		month2 = "Jan"
+	} else if (month === "2") {
+		month2 = "Feb"
+	} else if (month === "3") {
+		month2 = "Mar"
+	} else if (month === "4") {
+		month2 = "Apr"
+	} else if (month === "5") {
+		month = "May"
+	}else if (month === "6") {
+		month2 = "Jun"
+	}else if (month === "7") {
+		month2 = "Jul"
+	}else if (month === "8") {
+		month2 = "Aug"
+	}else if (month === "09") {
+		month2 = "Sep"
+;	}else if (month === "10") {
+		month2 = "Oct"
+	}else if (month === "11") {
+		month2 = "Nov"
+	}else if (month === "12") {
+		month2 = "Dec"
+	}
+	document.getElementById("weather").innerHTML =  month2 + " " +day;
+}, 1000)
 
+	
